d9061867bfc777cd52bb8c40273e33552f919845
Fedor <osinaid@mail.ru>
2025-04-03 18:06:43 +0500

    feat: fix header layout and add link to homepage

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,26 +1,47 @@
 "use client";
 
 import React from "react";
-import { AppBar, Box, Button, Toolbar, Typography } from "@mui/material";
+import {
+  AppBar,
+  Box,
+  Button,
+  Divider,
+  Toolbar,
+  Typography,
+} from "@mui/material";
 import { useSession, signIn, signOut } from "next-auth/react";
+import Link from "next/link";
 
 export const Header = () => {
   const { data: session } = useSession();
 
   return (
-    <AppBar position="static">
+    <AppBar
+      position="static"
+      color="transparent"
+      elevation={0}
+      sx={{ px: 4, py: 1 }}
+    >
       <Toolbar sx={{ display: "flex", justifyContent: "space-between" }}>
-        <Typography variant="h6">My Blog</Typography>
-        <Box>
+        <Box sx={{ display: "flex", alignItems: "center" }}>
+          <Link href="/" style={{ textDecoration: "none", color: "inherit" }}>
+            <Typography variant="h5" sx={{ fontWeight: "bold" }}>
+              .my_blog
+            </Typography>
+          </Link>
+          <Divider orientation="vertical" flexItem sx={{ mx: 2 }} />
+          {session && (
+            <Typography variant="body1" sx={{ ml: 1 }}>
+              {session.user?.name}
+            </Typography>
+          )}
+        </Box>
+
+        <Box sx={{ display: "flex", alignItems: "center" }}>
           {session ? (
-            <>
-              <Typography variant="body2" sx={{ mr: 2, display: "inline" }}>
-                {session.user?.name}
-              </Typography>
-              <Button color="inherit" onClick={() => signOut()}>
-                Выйти
-              </Button>
-            </>
+            <Button color="inherit" onClick={() => signOut()}>
+              Выйти
+            </Button>
           ) : (
             <Button color="inherit" onClick={() => signIn()}>
               Войти
